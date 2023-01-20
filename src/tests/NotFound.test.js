67bde759e298ddc9e447448df672a8f1463c9367
Luis <l-serrato@live.com>
2023-01-20 15:33:20 -0300

    req4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../pages/About';
+
+test('Testes About', () => {
+  render(<About />);
+  const aboutTitle = screen.getByRole('heading', {
+    level: 2,
+    name: 'Page requested not found',
+  });
+  expect(aboutTitle).toBeInTheDocument();
+});
+
+test('Testes img', () => {
+  render(<About />);
+  const aboutTitle = screen.getByRole('img', {
+    name: 'Pikachu crying because the page requested was not found',
+  });
+  expect(aboutTitle).toBeInTheDocument();
+  expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
