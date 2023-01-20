6089a46901ccfca98990d35fac47aa53724fd29f
Luis <l-serrato@live.com>
2023-01-20 16:13:29 -0300

    2?

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -14,15 +14,15 @@ test('Testes About', () => {
 
 test('Verificando se há descrição', () => {
   renderWithRouter(<About />);
-  const btn = screen.getAllByRole('paragraph');
-  expect(btn).toHaveLength(2);
+  const firstParagrah = screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémon');
+  expect(firstParagrah).toBeInTheDocument();
+  const secondParagrah = screen.getByText('One can filter Pokémon by type, and see more details for each one of them');
+  expect(secondParagrah).toBeInTheDocument();
 });
 
 test('Testes img', () => {
   renderWithRouter(<About />);
-  const aboutTitle = screen.getByRole('img', {
-    name: 'Pokédex',
-  });
-  expect(aboutTitle).toBeInTheDocument();
+  const img = screen.getByRole('img', { name: /pokédex/i });
+  expect(img).toBeInTheDocument();
   expect(img.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -16,6 +16,7 @@ test('Testes img', () => {
   renderWithRouter(<About />);
   const aboutTitle = screen.getByRole('img', {
     name: 'Pikachu crying because the page requested was not found',
+    src: 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
   });
   expect(aboutTitle).toBeInTheDocument();
   expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
