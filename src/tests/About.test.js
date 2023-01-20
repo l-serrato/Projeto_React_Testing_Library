e8f0fd32907923059a6eff6e07bd296e2810542a
Luis <l-serrato@live.com>
2023-01-20 15:41:01 -0300

    fix?

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import About from '../pages/About';
 
 test('Testes About', () => {
