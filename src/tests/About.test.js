48f7f19c602d46e2533f45b0582fe9d51c30ad92
Luis <l-serrato@live.com>
2023-01-20 15:22:49 -0300

    req2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../pages/About';
+
+test('Testes About', () => {
+  render(<About />);
+  const aboutTitle = screen.getByRole('heading', {
+    name: 'About Pokédex',
+  });
+  expect(aboutTitle).toBeInTheDocument();
+});
+
+test('Verificando se há descrição', () => {
+  render(<About />);
+  const btn = screen.getAllByRole('paragraph');
+  expect(btn).toHaveLength(2);
+});
+
+test('Testes img', () => {
+  render(<About />);
+  const aboutTitle = screen.getByRole('img', {
+    name: 'Pokédex',
+  });
+  expect(aboutTitle).toBeInTheDocument();
+  expect(img.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
