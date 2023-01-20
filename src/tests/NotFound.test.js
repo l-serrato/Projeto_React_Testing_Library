c1ba099f23bf65bef062a48d5c86bb2bdaa1e13f
Luis <l-serrato@live.com>
2023-01-20 16:37:34 -0300

    src fix

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -19,6 +19,6 @@ test('Testes img', () => {
   );
   expect(img).toHaveAttribute(
     'src',
-    'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
   );
 });
