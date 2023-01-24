5a2c9186b7ec7cae14b14b673c8bce60a7e5cd1b
Luis <l-serrato@live.com>
2023-01-23 22:14:45 -0300

    5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -4,55 +4,42 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-const name = 'pokemon-name';
+const encounteredTitle = () => screen.getByRole('heading', { name: /encountered pokémon/i });
+const nextPokemonBtn = () => screen.getByRole('button', { name: /próximo pokémon/i });
+const fireTypeBtn = () => screen.getByText(/fire/i);
+const psychicTypeBtn = () => screen.getAllByTestId('pokemon-type-button')[4];
+const nextBtn = () => screen.getByRole('button', {
+  name: /próximo pokémon/i,
+});
+const allBtn = () => screen.getByText(/all/i);
 
-test('teste cabeçalho', () => {
-  renderWithRouter(<App />);
-  const header = screen.getByRole('heading', {
-    level: 2,
-    name: 'Encountered Pokémon',
+describe('Testes no componente Pokedéx', () => {
+  it('Verifique se a a página contém um heading h2 com o texto "Encountered Pokémon"', () => {
+    renderWithRouter(<App />);
+
+    expect(encounteredTitle()).toBeInTheDocument();
   });
-  expect(header).toBeDefined();
-});
 
-test('button 1', () => {
-  renderWithRouter(<App />);
-  const btn1 = screen.getByRole('button', { name: /próximo pokémon/i });
-  expect(btn1).toBeInTheDocument();
-});
+  it('Verifica se o botão contém o texto "Próximo Pokémon"', () => {
+    renderWithRouter(<App />);
 
-it('é mostrado apenas um Pokémon por vez', () => {
-  renderWithRouter(<App />);
-  const pokemonName = screen.getAllByTestId(name);
-  expect(pokemonName).toHaveLength(1);
-});
+    expect(nextPokemonBtn()).toBeInTheDocument();
+    expect(nextPokemonBtn()).toHaveTextContent('Próximo Pokémon');
+  });
 
-test('filter buttons', () => {
-  renderWithRouter(<App />);
-  const nextBtn = screen.getByRole('button', { name: /Próximo Pokémon/i });
-  const btn2 = screen.getAllByTestId('pokemon-type-button');
-  expect(btn2).toHaveLength(7);
-  const btnFire = screen.getByRole('button', { name: 'Fire' });
-  expect(btnFire).toBeInTheDocument();
-  const btnPsych = screen.getByRole('button', { name: 'Psychic' });
-  expect(btnPsych).toBeInTheDocument();
-  userEvent.click(btnFire);
-  const pokemonFire = screen.getByText(/Charmander/i);
-  expect(pokemonFire).toBeInTheDocument();
-  userEvent.click(btnPsych);
-  const pokemonPsych = screen.getByText(/Alakazam/i);
-  expect(pokemonPsych).toBeInTheDocument();
-  userEvent.click(nextBtn);
-  expect(screen.getByTestId(name)).toHaveTextContent(/mew/i);
-  const btn3 = screen.getByText(/All/i);
-  expect(btn3).toBeInTheDocument();
-});
+  it('Verifica se é exibido o próximo Pokémon da lista ao clicar no botão', () => {
+    renderWithRouter(<App />);
 
-test('all button', () => {
-  renderWithRouter(<App />);
-  const btn3 = screen.getByText(/All/i);
-  expect(btn3).toBeInTheDocument();
-  userEvent.click(btn3);
-  const pokemonName = screen.getByTestId(name);
-  expect(pokemonName).toHaveTextContent('Pikachu');
+    userEvent.click(fireTypeBtn());
+    expect(screen.getByText(/charmander/i)).toBeInTheDocument();
+
+    userEvent.click(psychicTypeBtn());
+    expect(screen.getByTestId('pokemon-name')).toHaveTextContent(/alakazam/i);
+
+    userEvent.click(nextBtn());
+    expect(screen.getByTestId('pokemon-name')).toHaveTextContent(/mew/i);
+
+    userEvent.click(allBtn());
+    expect(screen.getByText(/pikachu/i)).toBeInTheDocument();
+  });
 });
